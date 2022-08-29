810844ae5599fe55877d1568cb6c69b39991bdc1
Kuba <jpiskozub@gmail.com>
2022-08-30 00:15:20 +0200

    Basic visualization added

--- handpy_mockup.py
+++ handpy_mockup.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 
 mqttc=mqtt.Client(client_id='handpi')
 
-broker= '192.168.0.101'
+broker= '192.168.0.100'
 port=1883
 topic='handpi'
 
@@ -10,7 +10,7 @@ topic='handpi'
 mqttc.connect(broker,port)
 
 try:
-    while True:
+    for i in range(0,10000):
         msg = (25905, 11909, 18324, 18330, 20017, 18343, 20499, 16829, 20308, 15385, 133.625, -1.4375, -161.0, 0.01, -0.01, 0.12, 0.001090830782496456, 0.002181661564992912, 0.001090830782496456, 42.75, -44.25, 30.25, -0.24, 3.16, -9.14)
         print (msg)
         mqttc.publish(topic,str(msg))
--- handvis.py
+++ handvis.py
@@ -3,7 +3,6 @@ import paho.mqtt.client as mqtt
 
 import dearpygui.dearpygui as dpg
 
-import plotly.express as px
 
 from queue import Queue
 
@@ -13,15 +12,58 @@ topic = 'handpi'
 
 mqttqueue = Queue()
 
+ADC_channels=['P1_1', 'P1_2', 'P2_1', 'P2_2', 'P3_1', 'P3_2', 'P4_1', 'P4_2', 'P5_1', 'P5_2']
+IMU_channels = ['Euler_x', 'Euler_y', 'Euler_z', 'Acc_x', 'Acc_y', 'Acc_z']
+
+sign_types = ['static', 'dynamic']
+sign_types_dict = {'a': sign_types[0],
+                   'ą': sign_types[1],
+                   'b': sign_types[0],
+                   'c': sign_types[0],
+                   'ć': sign_types[1],
+                   'ch': sign_types[1],
+                   'cz': sign_types[1],
+                   'd': sign_types[1],
+                   'e': sign_types[0],
+                   'ę': sign_types[1],
+                   'f': sign_types[1],
+                   'g': sign_types[1],
+                   'h': sign_types[1],
+                   'i': sign_types[0],
+                   'j': sign_types[1],
+                   'k': sign_types[1],
+                   'l': sign_types[0],
+                   'ł': sign_types[1],
+                   'm': sign_types[0],
+                   'n': sign_types[0],
+                   'ń': sign_types[1],
+                   'o': sign_types[0],
+                   'ó': sign_types[1],
+                   'p': sign_types[0],
+                   'r': sign_types[0],
+                   'rz': sign_types[1],
+                   's': sign_types[0],
+                   'ś': sign_types[1],
+                   'sz': sign_types[1],
+                   't': sign_types[0],
+                   'u': sign_types[0],
+                   'w': sign_types[0],
+                   'y': sign_types[0],
+                   'z': sign_types[1],
+                   'ź': sign_types[1],
+                   'ż': sign_types[1]}
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     
     
 def on_message(client, userdata, message):
+    
     mqttqueue.put(message.payload.decode('utf-8'))
+    #dpg.render_dearpygui_frame()
   
 def on_disconnect(client, userdata,rc=0):
-    print("DisConnected result code "+str(rc))
+    print("Disconnected result code "+str(rc))
     client.loop_stop()
     
 def main():
@@ -29,37 +71,49 @@ def main():
     mqttc.connect(broker, port)
     mqttc.on_connect = on_connect
     mqttc.on_message = on_message
+    mqttc.on_disconnect = on_disconnect
     
     mqttc.subscribe(topic)    
     
     dpg.create_context()
-    dpg.create_viewport(title='Custom Title', width=600, height=200)
+    dpg.create_viewport(title='HandPi', width=600, height=200)
     
-    with dpg.window(label="Example Window"):
-        dpg.add_text("Hello, world")
+    with dpg.window(label="Flex sensors"):
+        with dpg.plot(label="Bar Series", height=-1, width=-1):
+            dpg.add_plot_axis(dpg.mvXAxis, label="Sensor", no_gridlines=False)
+            dpg.set_axis_ticks(dpg.last_item(), (("P1_1", 11), ("P1_2", 21), ("P2_1", 31), ("P2_2", 42), ("P3_1",51), ("P3_2",61), ("P4_1",71), ("P4_2",81), ("P5_1",91), ("P5_2", 101)))
+            dpg.add_plot_axis(dpg.mvYAxis, label="Value", tag="yaxis_tag")
+            dpg.add_bar_series([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], [10, 10, 10, 10, 10, 10, 10, 10, 10, 10], weight=1, parent="yaxis_tag", tag = 'vals')
+
         
-   
+    dpg.show_metrics()
     dpg.setup_dearpygui()
     dpg.show_viewport()
-    dpg.start_dearpygui()
+    mqttc.loop_start()
+ 
 
-   
+    
     while dpg.is_dearpygui_running():
         try:
-            mqttc.loop_start()
-            dpg.add_text(str(mqttqueue.get()))
+            
+            
+            
+            vals = eval(mqttqueue.get())
+                
+            dpg.set_value('vals', [[10, 20, 30, 40, 50, 60, 70, 80, 90, 100], vals[0:9]])
+            
+            print(vals)
 
             dpg.render_dearpygui_frame()
             
         except KeyboardInterrupt:
             print ('Interrupted')
-            mqttc.loop_stop()
-            mqttc.disconnect()
+            mqttc.loop_stop(force=True)
     else:
         mqttc.loop_stop()
         mqttc.disconnect()
            
-
+    dpg.destroy_context()
     
     
 
